dc79314f45470d804fc34ee981df5cd123049d41
bartekk3007 <149260538+bartekk3007@users.noreply.github.com>
2024-06-29 23:52:00 +0200

    Add files via upload

---
+++ PeriodicTimerInCSharp/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace PeriodicTimerInCSharp
+{
+    public class Program
+    {
+        public static PeriodicTimer periodicTimer = new PeriodicTimer(new TimeSpan(0, 0, 5));
+        static async Task Main(string[] args)
+        {
+            while (await periodicTimer.WaitForNextTickAsync())
+            {
+                Console.WriteLine(DateTime.Now.ToString("o"));
+            }
+
+        }
+    }
+}
